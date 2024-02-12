64d2460514a2fa660c0ebfad59ad005e2a24de26
bl815v <133074441+bl815v@users.noreply.github.com>
2024-02-12 15:02:16 -0500

    correcion boton v1

--- funcion.js
+++ funcion.js
@@ -1,11 +1,11 @@
-function guardarDato(){
+function guardarDatos(){
     var nombre = document.getElementById("nombre1").value
     localStorage.setItem('nombre', nombre);
     localStorage.setItem('porcentaje', porcentaje);
     window.location.href = 'boton.html';
 }
 
-function obtenerDato(){
+function obtenerDatos(){
     var nombre = localStorage.getItem('nombre');
     var porcentaje = localStorage.getItem('porcentaje');
 
