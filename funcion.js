d5ab6b3e64bef0bf18fd0f581d13331c80df05bd
bl815v <133074441+bl815v@users.noreply.github.com>
2024-02-12 14:59:57 -0500

    pruebaBoton

--- funcion.js
+++ funcion.js
@@ -2,12 +2,13 @@ function guardarDato(){
     var nombre = document.getElementById("nombre1").value
     localStorage.setItem('nombre', nombre);
     localStorage.setItem('porcentaje', porcentaje);
+    window.location.href = 'boton.html';
 }
 
 function obtenerDato(){
     var nombre = localStorage.getItem('nombre');
     var porcentaje = localStorage.getItem('porcentaje');
-    
+
     document.getElementById('nombreGuardado').textContent = nombre;
     document.getElementById('porcentajeGuardado').textContent = porcentaje;
 }
