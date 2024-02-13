9e800f45b95c7185f615fe6de85e6f2bb67bd1f5
bl815v <133074441+bl815v@users.noreply.github.com>
2024-02-13 15:15:30 -0500

    prueba

--- funcion.js
+++ funcion.js
@@ -2,6 +2,8 @@ function guardarDatos(){
     var nombre = document.getElementById("nombre1").value
     localStorage.setItem('nombre', nombre);
     window.location.href = 'boton.html';
+    document.getElementById('nombreGuardado').textContent = nombre;
+
 }
 
 function obtenerDatos(){
