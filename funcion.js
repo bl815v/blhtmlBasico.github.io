614850b8a5dc7c3fd29d1a406396e3b3deaed8bb
bl815v <133074441+bl815v@users.noreply.github.com>
2024-02-14 17:49:09 -0500

    funcional por url

--- funcion.js
+++ funcion.js
@@ -1,13 +1,33 @@
-function guardarDatos(){
-    var nombre = document.getElementById("nombre1").value
-    localStorage.setItem('nombre', nombre);
-    window.location.href = 'boton.html';
-    document.getElementById('nombreGuardado').textContent = nombre;
+function guardarDatos() {
+    var datos = [];
+    for (var i = 1; i <= 5; i++) {
+        var nombre = document.getElementById("nombre" + i).value;
+        var porcentaje = document.getElementById("Porcentaje" + i).value;
+        datos.push({ nombre: nombre, porcentaje: porcentaje });
+    }
+
+    var parametrosUrl = '';
+    for (var j = 0; j < datos.length; j++) {
+        if (datos[j].nombre && datos[j].porcentaje) {
+            parametrosUrl += '&nombre' + (j + 1) + '=' + encodeURIComponent(datos[j].nombre);
+            parametrosUrl += '&Porcentaje' + (j + 1) + '=' + encodeURIComponent(datos[j].porcentaje);
+        }
+    }
+
+    window.location.href = 'boton.html?' + parametrosUrl.slice(1);
 
 }
 
-function obtenerDatos(){
-    var nombre = localStorage.getItem('nombre');
 
-    document.getElementById('nombreGuardado').textContent = nombre;
+document.addEventListener('DOMContentLoaded', obtenerDatos);
+
+
+function obtenerDatos() {
+    var parametrosUrl = new URLSearchParams(window.location.search);
+    for (var i = 1; i <= 5; i++) {
+        var nombre = parametrosUrl.get('nombre' + i);
+        var porcentaje = parametrosUrl.get('Porcentaje' + i);
+        document.getElementById('nombre' + i).textContent = nombre || '';
+        document.getElementById('Porcentaje' + i).textContent = (porcentaje || '') + '%';
+    }
 }
