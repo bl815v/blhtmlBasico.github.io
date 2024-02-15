753f3c80c40e66b904657da274396fbb85b1c694
bl815v <133074441+bl815v@users.noreply.github.com>
2024-02-14 22:26:56 -0500

    funcional estandarizado

---
+++ funcionGuardar.js
@@ -0,0 +1,24 @@
+function guardarDatos() {
+    var datos = [];
+    for (var i = 1; i <= 5; i++) {
+        var nombre = document.getElementById("nombre" + i).value;
+        var porcentaje = document.getElementById("Porcentaje" + i).value;
+        datos.push({ nombre: nombre, porcentaje: porcentaje });
+    }
+
+    var parametrosUrl = '';
+    for (var j = 0; j < datos.length; j++) {
+        if (datos[j].nombre && datos[j].porcentaje) {
+            parametrosUrl += '&nomb' + (j + 1) + '=' + encodeURIComponent(datos[j].nombre);
+            parametrosUrl += '&Porc' + (j + 1) + '=' + encodeURIComponent(datos[j].porcentaje);
+        }
+    }
+
+    window.location.href = 'boton.html?' + parametrosUrl.slice(1);
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const boton1 = document.getElementById("guardar");
+    boton1.addEventListener("click", guardarDatos);
+});
